2e9266cce3de4e3a86a9cfee118e332c2b0b3e9f
Francisco Savala <franciscosavala01@gmail.com>
2023-12-26 19:45:40 -0300

    otro mass

--- app.js
+++ app.js
@@ -4,25 +4,6 @@ const hola = () => {
 console.log("UPS FUE UN ERROR!");
 console.log("voy a probar algo de los branch");
 console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
 
 console.log("ESTO ES OTRA PRUEBA!");
 console.log("ESTO ES OTRA PRUEBA!");
