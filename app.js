129c66e1a6c07a1c9747e3ba9a2f027c0cf27eef
Francisco Savala <franciscosavala01@gmail.com>
2023-12-27 18:54:15 -0300

    main1

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const hola = () => {
 }
 console.log("UPS FUE UN ERROR!");
 console.log("voy a probar algo de los branch");
-console.log("ESTO ES OTRA PRUEBA!");
-
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("AGREGADO OTRA COSA");
-console.log("AGREGADO OTRA COSA");
-console.log("AGREGADO OTRA COSA");
-console.log("AGREGADO OTRA COSA");
+console.log("voy a probar algo de los branch");
+console.log("voy a probar algo de los branch");
+console.log("voy a probar algo de los branch");
+console.log("voy a probar algo de los branch");
+console.log("voy a probar algo de los branch");
 hola();
