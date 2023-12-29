3319367f0239f713d94b8c005e2a66f94f389e6d
Francisco Savala <franciscosavala01@gmail.com>
2023-12-28 23:53:53 -0300

    prueba reset

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 const hola = () => {
     console.log('Hola mundo');
 }
-console.log("UPS FUE UN ERROR!");
-console.log("voy a probar algo de los branch");
-console.log("ESTO ES OTRA PRUEBA!");
-
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("AGREGADO Ogasgs");
-console.log("AGREGADO OTRA COSA");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
-console.log("ESTO ES OTRA PRUEBA!");
 
 hola();
